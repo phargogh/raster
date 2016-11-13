c8a9a43ce35c80dd38f4f7199927e2626860ba62
James Douglass <jamesdouglassusa@gmail.com>
2016-11-13 12:05:34 -0800

    Adding iterblocks demo file.

---
+++ _episodes/05-pyGeoprocessing/iterblocks-demo.py
@@ -0,0 +1,7 @@
+# approximately what percentage area of yosemite national park is above 3500m (11482 ft.)?
+
+import pygeoprocessing
+
+# rasterize yosemite vector to create new layer
+# use collected DEM from previous example.
+# use iterblocks to calculate the within the park and the area above 3500m.
